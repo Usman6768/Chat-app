97d02f7be44b1a5b548826d91d2bf722897b166d
Usman Pervez <usmanpervez5@gmail.com>
2025-03-28 17:56:01 +0500

    Add frontend pages and routing, integrate Tailwind CSS, and implement CORS in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser"
+import cors from 'cors'
 
 import {connectDB} from "./lib/db.js"
 import authRoutes from "./routes/auth.route.js"
@@ -12,6 +13,10 @@ const app = express();
 
 app.use(express.json()) // we write this middleware because we have to take data from user in controllers like (req.body) and this will allow us to extract the json data from req.body
 app.use(cookieParser()) // we use this middleware to parse the cookies sent by the client to the server
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true // we write this to allow the server to send cookies to the client
+}))
 
 const PORT = process.env.PORT;
 
