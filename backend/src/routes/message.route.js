a205bc79063f7c2d1079ad92435d51eb93ee16ad
Usman Pervez <usmanpervez5@gmail.com>
2025-03-28 12:58:11 +0500

    Fix imports in Message routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express"
-import protectRoute from "../middlewares/auth.middleware.js"
-import { getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {protectRoute} from "../middlewares/auth.middleware.js"
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
